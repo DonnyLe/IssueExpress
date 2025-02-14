475a7652883b8ad284e59b6554cb287c69d37c8e
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2025-02-14 00:44:08 -0500

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -23,7 +23,7 @@ async function signInWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: getURL(),
+      redirectTo: `${getURL()}auth/callback`,
     },
   });
   return { data, error };
