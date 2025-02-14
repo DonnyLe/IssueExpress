8441b899e832a5fe7d15aee61cfc9c85fc691927
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2025-02-14 00:17:59 -0500

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -8,9 +8,9 @@ import { Project } from "./components/project-selector";
 async function signInWithGithub() {
   const supabase = await createClient();
 
-  const defaultUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
+  // const defaultUrl = process.env.VERCEL_URL
+  //   ? `https://${process.env.VERCEL_URL}`
+  //   : "http://localhost:3000";
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
